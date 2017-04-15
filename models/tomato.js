7861caf5dae2cbd9f907f29e119fe3716778c862
Jose Alava <jalava85@gmail.com>
2017-04-14 20:34:58 -0400

    Adjusting to new structure...

--- models/tomato.js
+++ models/tomato.js
@@ -5,8 +5,8 @@ var vogels = require('vogels-promisified'),
     config = require('../config');
 vogels.AWS.config.update({region: config.region});
 
-var modelName = 'Property',
-    tableName = 'Properties',
+var modelName = 'Tomato',
+    tableName = 'Tomatoes',
 
     Joi = require('joi'),
     Booking = vogels.define(modelName, {
@@ -17,27 +17,8 @@ var modelName = 'Property',
 
         schema: {
             id: vogels.types.uuid(),
-            altId: Joi.string(),
-            bpId: Joi.string(),
-            name: Joi.string(),
-            lat: Joi.string(),
-            long: Joi.string(),
-            city: Joi.string(),
-            state: Joi.string(),
-            country: Joi.string(),
-            destinationId: Joi.string(),
-            townId: Joi.string(),
-            communityId: Joi.string(),
-            photos: [Joi.string(), Joi.string()],
-            description: Joi.string(),
-            houseInformation: Joi.string(),
-            guests: Joi.string(),
-            rating: Joi.string(),
-            bedrooms: Joi.string(),
-            bathrooms: Joi.string(),
-            neighborhood: Joi.string(),
-            propertyType: Joi.string(),
-            amenities: Joi.string()
+            author: Joi.string(),
+            description: Joi.string()
         },
 
         tableName: tableName
--- routes/tomato/index.js
+++ routes/tomato/index.js
@@ -1,10 +1,10 @@
 /**
  * Created by jalava on 3/22/2017.
  */
-var db = require('../../../repo'),
-    Property = require('../../../models/property'),
+var db = require('../../repo'),
+    Property = require('../../models/tomato'),
     repo = new db(Property),
-    crudSvc = new require('../../../crudSvc')(repo),
-    router = new require('../../../router')(crudSvc.get, crudSvc.post, crudSvc.put, crudSvc.del);
+    crudSvc = new require('../../crudSvc')(repo),
+    router = new require('../../router')(crudSvc.get, crudSvc.post, crudSvc.put, crudSvc.del);
 
 module.exports.main = router;
