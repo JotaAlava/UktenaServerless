34d63d223447640b37b726603b2a5617e1c8caae
Jose Alava <jalava85@gmail.com>
2017-04-23 20:08:02 -0400

    Fixed the pre-Op to update the author correctly, but something happened with the dates...

--- routes/tomato/index.js
+++ routes/tomato/index.js
@@ -1,16 +1,24 @@
-var tomatoSorter = require('./tomatoSorter');
-var postOp = {
-  get: tomatoSorter
-};
 /**
  * Created by jalava on 3/22/2017.
  */
+var tomatoSorter = require('./tomatoSorter'),
+  postOp = {
+    get: tomatoSorter
+  },
+  preOps = {
+    post: function (options) {
+      console.log(options.body);
+      options.body.author = options.event.principalId;
+      console.log(options.body);
+
+      return true;
+    }
+  };
+
 var db = require('../../repo'),
   Property = require('../../models/tomato'),
   repo = new db(Property),
-  crudSvc = new require('../../crudSvc')(repo, {post: function () {
-
-  }}, postOp),
+  crudSvc = new require('../../crudSvc')(repo, preOps, postOp),
   router = new require('../../router')(crudSvc.get, crudSvc.post, crudSvc.put, crudSvc.del);
 
 module.exports.main = router;
